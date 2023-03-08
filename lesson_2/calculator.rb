42bdf5c95808cd35f6d7872b9e64407ea44f3de9
Shawn Grycki <sglightning@gmail.com>
2023-03-07 22:32:42 -0600

    Adds multiple language support with config file

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,72 +1,81 @@
+LANGUAGE = 'en'
+
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
+def messages(message, lang)
+  MESSAGES[lang][message]
+end
+
 def prompt(message)
   puts("=> #{message}")
 end
 
-def valid_number?(num)
-  num.to_i != 0
+def integer?(num)
+  num.to_i.to_s == num
+end
+
+def float?(num)
+  num.to_f.to_s == num
+end
+
+def number?(input)
+  integer?(input) || float?(input)
 end
 
 def operation_to_message(op)
   case op
   when '1'
-    'Adding'
+    MESSAGES[LANGUAGE]['add']
   when '2'
-    'Subtracting'
+    MESSAGES[LANGUAGE]['subtract']
   when '3'
-    'Multiplying'
+    MESSAGES[LANGUAGE]['multiply']
   when '4'
-    'Dividing'
+    MESSAGES[LANGUAGE]['divide']
   end
 end
 
-prompt("Welcome to Calculator! Enter your name:")
+prompt(messages('welcome', LANGUAGE))
 
 name = nil
 loop do
   name = gets.chomp
 
   if name.empty?
-    prompt("Make sure to use a valid name.")
+    prompt(MESSAGES[LANGUAGE]['valid_name'])
   else
     break
   end
 end
 
-prompt("Hi #{name}!")
+prompt(MESSAGES[LANGUAGE]['greet'] + "#{name}!")
 loop do # main loop
   number1 = nil
   loop do
-    prompt("What's the first number?")
+    prompt(MESSAGES[LANGUAGE]['first'])
     number1 = gets.chomp
 
-    if valid_number?(number1)
+    if number?(number1)
       break
     else
-      prompt("Hmm... that doesn't look like a valid number")
+      prompt(MESSAGES[LANGUAGE]['valid_num'])
     end
   end
 
   number2 = nil
   loop do
-    prompt("What's the second number?")
+    prompt(MESSAGES[LANGUAGE]['second'])
     number2 = gets.chomp
 
-    if valid_number?(number2)
+    if number?(number2)
       break
     else
-      prompt("Hmm... that doesn't look like a valid number")
+      prompt(MESSAGES[LANGUAGE]['valid_num'])
     end
   end
 
-  operator_prompt = <<-MSG # Heredoc for multi-line strings
-    What operation would you like to perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
-  MSG
-
-  prompt(operator_prompt)
+  prompt(MESSAGES[LANGUAGE]['operator'])
 
   operator = nil
   loop do
@@ -74,28 +83,28 @@ loop do # main loop
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt("Must choose 1, 2, 3 or 4")
+      prompt(MESSAGES[LANGUAGE]['valid_operator'])
     end
   end
 
-  prompt("#{operation_to_message(operator)} the two numbers...")
+  prompt(operation_to_message(operator))
 
   result = case operator
            when '1'
-             number1.to_i + number2.to_i
+             number1.to_f + number2.to_f
            when '2'
-             number1.to_i - number2.to_i
+             number1.to_f - number2.to_f
            when '3'
-             number1.to_i * number2.to_i
+             number1.to_f * number2.to_f
            when '4'
              number1.to_f / number2.to_f
            end
 
-  prompt("The result is #{result}")
+  prompt(MESSAGES[LANGUAGE]['result'] + result.to_s)
 
-  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  prompt(MESSAGES[LANGUAGE]['again'])
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt("Thank you for using the calculator. Good bye!")
+prompt(MESSAGES[LANGUAGE]['farewell'])
