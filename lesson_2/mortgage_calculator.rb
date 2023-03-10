a34864416867da87ace7daa5cf11b02423c06c27
Shawn Grycki <sglightning@gmail.com>
2023-03-09 18:03:35 -0600

    Enhances mortgage calculator user-experience

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -1,3 +1,6 @@
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_calculator_messages.yml')
+
 # Method Definitions
 
 # Puts messages with => prefix
@@ -23,18 +26,19 @@ end
 # Calculates the monthly interest rate
 def monthly_rate(yearly_rate)
   yearly_rate = yearly_rate.to_f
-  yearly_rate / 1200 # (100 * 12)
+  yearly_rate / (RATE_MULTIPLIER * MONTH_NUMBER)
 end
 
 # Calculates total loan duration in months
 def loan_duration(years, months)
   years = years.to_i
   months = months.to_i
-  ((years * 12) + months)
+  ((years * MONTH_NUMBER) + months)
 end
 
 # Calculates monthly payment amount
 def monthly_payment(principal, monthly_rate, duration)
+  return principal / duration if monthly_rate.zero?
   principal * (monthly_rate / (1 - (1 + monthly_rate)**(-duration)))
 end
 
@@ -48,54 +52,85 @@ def total_amount(principal, total_interest)
   principal + total_interest
 end
 
+# Loop prompt for inputs (Doesn't work. Inf + NaN error)
+def loop_prompt(key, var)
+	 loop do
+		  print_message(MESSAGES[key])
+		  var = gets.chomp
+		  if var.empty? || number?(var) == false
+				  print_message(MESSAGES['error'])
+				  next
+		  else
+				  break
+		  end
+	 end
+end
+
+# Constant variable declarations
+RATE_MULTIPLIER = 100
+MONTH_NUMBER = 12
+
 # Variable declarations for loop block
 principal = nil
 years = nil
 months = nil
 rate = nil
+ans = nil
+
+print_message(MESSAGES['welcome'])
+print_message(MESSAGES['lines'])
 
 # Main Program
 loop do
-  print_message("Welcome to Mortgage Calculator!")
-  print_message("-------------------------------")
 
   loop do
-    print_message("Please enter the principal amount")
+    print_message(MESSAGES['principal_prompt'])
     principal = gets.chomp
     if principal.empty? || number?(principal) == false
-      print_message("Please enter a valid number")
+      print_message(MESSAGES['error'])
       next
     else
-      principal = principal.to_f
+					 principal = principal.to_f.round(2)
+						break
     end
+		end
 
-    print_message("Please enter the number of years in loan duration")
+		loop do
+    print_message(MESSAGES['years_prompt'])
     years = gets.chomp
-    if years.empty? || number?(years) == false
-      print_message("Please enter a valid number")
+    if years.empty? || integer?(years) == false
+      print_message(MESSAGES['error'])
       next
+				else
+					 break
     end
+  end
 
-    print_message("Please enter the additional months after years")
+  loop do
+    print_message(MESSAGES['months_prompt'])
     months = gets.chomp
-    if months.empty? || number?(months) == false
-      print_message("Please enter a valid number")
+    if months.empty? || integer?(months) == false
+      print_message(MESSAGES['error'])
       next
+				else
+					 break
     end
+			end
 
-    print_message("Please enter the annual percentage rate")
+		loop do
+    print_message(MESSAGES['apr_prompt'])
     rate = gets.chomp
     if rate.empty? || number?(rate) == false
-      print_message("Please enter a valid number")
+      print_message(MESSAGES['error'])
       next
+				else
+					 break
     end
-
-    break
-  end
+			end
 
   # Method calls
   mrate = monthly_rate(rate)
-  print_message("Monthly Rate " + (mrate * 100).round(4).to_s + "%")
+  print_message("Monthly Rate " + (mrate * RATE_MULTIPLIER).round(4).to_s + "%")
 
   duration = loan_duration(years, months)
   print_message("Total of #{duration} Payments")
@@ -109,11 +144,22 @@ loop do
   amount = total_amount(principal, interest)
   print_message("Total Payment $#{format('%.2f', amount)}")
 
-  print_message("--------------------------------------------------")
-  print_message("Another Calculation? (y or yes to calculate again)")
-  ans = gets.chomp
-  break unless ans.downcase.start_with?('y', 'yes')
+  print_message(MESSAGES['lines2'])
+
+  
+		loop do
+			 print_message(MESSAGES['another?'])
+			 ans = gets.chomp
+    if ans.downcase.start_with?('y', 'yes')
+					 break
+		  elsif ans.downcase.start_with?('n', 'no')
+			   break
+		  else
+		    print_message(MESSAGES['error'])
+		  end
+		end
+		break if ans.downcase.start_with?('n', 'no')
 end
 
-print_message("Thank you for using the Mortgage Calculator!")
-print_message("Good Bye!")
+print_message(MESSAGES['thanks'])
+print_message(MESSAGES['goodbye'])
